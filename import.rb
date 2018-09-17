3c076414be37d35042dda56adbf51ef6cafb5fa7
George H. Peden <georgep@slalom.com>
2018-09-17 14:52:35 +0100

    get account id's from ENV

--- import.rb
+++ import.rb
@@ -1,13 +1,6 @@
 # Account #1
 Import::Teller.new(
   ENV['TELLER_TOKEN'],
-  "f38c26fc-29af-4087-875b-030bc3765936",
-  "14d7e25f-0d8f-4a16-b2a4-1383174f4737"
-).import
-
-# Account #2
-Import::Teller.new(
-  ENV['TELLER_TOKEN'],
-  "54aeeef5-0dfa-4af0-b714-9af78ed27c79",
-  "0eba281d-3e10-4e43-be83-3187a3cf1e9b"
+  ENV['TELLER_ACCOUNT_ID1'],
+  ENV['YNAB_ACCOUNT_ID1']
 ).import
